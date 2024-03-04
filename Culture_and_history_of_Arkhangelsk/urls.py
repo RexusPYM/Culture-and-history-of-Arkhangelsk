4f673022221d467b231dbb170ba2e9797083858e
RexusPYM <praushkov.d@gmail.com>
2024-03-04 18:16:23 +0300

    added system of register and login

--- Culture_and_history_of_Arkhangelsk/urls.py
+++ Culture_and_history_of_Arkhangelsk/urls.py
@@ -3,12 +3,17 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', views.CommonSearchListView.as_view(), name='home'),
     path('historical_object/', include('historical_objects.urls')),
+    path('login/', views.LoginUser.as_view(), name='login'),
+    path('exit/', auth_views.LogoutView.as_view(next_page='home'), name='exit'),
+    path('register/', views.RegisterUser.as_view(), name='register'),
+    path('user_profiles/', include('user_profiles.urls')),
 ]
 
 # нужно что-бы показывались картинки
--- Culture_and_history_of_Arkhangelsk/views.py
+++ Culture_and_history_of_Arkhangelsk/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
 from historical_objects.models import HistoricalObject
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from historical_objects.utils import *
+from historical_objects.forms import *
 
 
 class CommonSearchListView(ListView):
@@ -41,3 +45,28 @@ class CommonSearchListView(ListView):
         return query_set
 
 
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = "register.html"
+    success_url = reverse_lazy("home")
+    context_object_name = "ctx"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        ctx = {"title": "Регистрация"}
+
+        return dict(list(context.items()) + list(ctx.items()))
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = AuthenticationForm
+    template_name = "login.html"
+
+    def get_success_url(self):
+        return reverse_lazy("home")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        ctx = {"title": "Аутентификация"}
+
+        return dict(list(context.items()) + list(ctx.items()))
